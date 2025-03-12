040dc1e99fe2db727c9ea9f3b682361a5777469d
Quentin Stempfer <quentin.stempfer@supdevinci-edu.fr>
2025-03-12 10:51:52 +0100

    httpError in UserControl

--- server/users/src/controllers/UserControl.py
+++ server/users/src/controllers/UserControl.py
@@ -2,6 +2,7 @@ from typing import Optional, List
 from users.src.services.UserService import UserService
 from users.src.data.models.User import User
 from utils.checkInfos import CheckInfos
+from ninja.errors import HttpError
 
 class UsersControl:
 
@@ -12,7 +13,7 @@ class UsersControl:
     @staticmethod
     def getByEmail(email: str) -> Optional[User]:
         if not CheckInfos.isEmail(email):
-            return "Invalid email"
+            raise HttpError(500, "Invalid email")
         return UserService.getByEmail(email)
 
     @staticmethod
@@ -22,19 +23,19 @@ class UsersControl:
     @staticmethod
     def update(data) -> Optional[User]:
         if not CheckInfos.isValideId(data.id):
-            return "Invalid id"
+            raise HttpError(500, "Invalid id")
         if not CheckInfos.isValideString(data.firstName):
-            return "Invalid firstName"
+            raise HttpError(500, "Invalid firstName")
         if not CheckInfos.isValideString(data.lastName):
-            return "Invalid lastName"
+            raise HttpError(500, "Invalid lastName")
         if not CheckInfos.isEmail(data.email):
-            return "Invalid email"
+            raise HttpError(500, "Invalid email")
         return UserService.update(data.id, data.firstName, data.lastName, data.email)
 
     @staticmethod
     def updatePassword(data) -> Optional[User]:
         if not CheckInfos.isValideId(data.id) or not CheckInfos.isValidPassword(data.password):
-            return "Invalid password"
+            raise HttpError(500, "Invalid password")
         return UserService.updatePassword(data.id, data.password)
 
     @staticmethod
